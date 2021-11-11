d515a19bc24d85ff80733b39bd8ad957f301a176
Winnie Lin <winnie50431@gmail.com>
2021-11-11 14:26:37 +0800

    merge

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 The F2E 網頁開發紀錄。
 
-想練切版和複習原生 JS ，所以沒有套框架。但為了功能開發方便，還是引入了 axios。
+【學習目標】
 
-#手刻 RWD #axios #TPX API
+1. 手刻 RWD
+2. 應用 TPX API (引入 axios)
+   - 顯示資料
+   - 搜尋功能
+3. 定位功能
 
 ### 頁面
 
@@ -14,15 +18,35 @@ The F2E 網頁開發紀錄。
 
 ---
 
-## 首頁
+## 功能規劃
 
-## 交通景點
+#### 首頁
 
-#### 功能規劃
+1. 顯示景點
+2. 依條件搜尋，顯示景點
+
+#### 交通景點
 
 1. 選擇縣市、公車路線
-   - 依選擇**縣市**，取得縣市相應的**公車路線**。
+   - 依選擇 **【縣市】**，取得縣市相應的 **【公車路線】**。 (11.8 checked)
 2. 搜尋，顯示車次與到站時間
-   - 依選擇*路線*，取得相應*站牌*
-   - 依站牌，配對*到站時間*
-   - 渲染畫面
+   - 依選擇 **【公車路線】**，取得相應 **【站牌】**。 (11.8 checked)
+   - 區分 _往返路線_ 的站牌。
+   - 依站牌，配對 **【到站時間】**。
+   - 渲染畫面。
+
+---
+
+### 筆記
+
+Fetch API vs. Axios
+
+- **Fetch API**
+  回傳 Promise → 使用 Response 物件 → 用 method(ex. json())獲得 body
+- **Axios**
+  回傳 Promise ，且與 Response & Error 物件一同被解析。
+
+參考資料:
+
+- [Fetch 還是 Axios——哪個更適合 HTTP 請求？\_杭州程式設計師張張 - MdEditor](https://www.gushiciku.cn/pl/pjgD/zh-tw)
+- [支援 OData 查詢語法](https://motc-ptx-api-documentation.gitbook.io/motc-ptx-api-documentation/api-te-se/odata#odata-yu-fa)
--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-let city, routeName;
-
-// 縣市資料
 const cityData = [
   { name: "臺北市", value: "Taipei" },
   { name: "新北市", value: "NewTaipei" },
@@ -26,124 +23,333 @@ const cityData = [
   { name: "連江縣", value: "LienchiangCounty" },
 ];
 
-// 選取地區
+// form data
 const citySelect = document.querySelector("#citySelect");
-function select() {
+const typeSelect = document.querySelector("#typeSelect");
+const input = document.querySelector("input#keywords");
+const search = document.querySelector("#search");
+// layout
+const topic = document.querySelector("section.topic");
+const allTopics = document.querySelectorAll("section.topic");
+// cards
+const mapCards = document.querySelector("div.cards_map");
+const foodCards = document.querySelector("div.cards_food");
+const hotelCards = document.querySelector("div.cards_hotel");
+const activityCards = document.querySelector("div.cards_activity");
+
+let url, city, type, keywords, limitNum;
+
+/** 縣市列表 */
+function getCity() {
   let str = '<option value="">選擇縣市</option>';
   cityData.forEach((item) => {
     str += `<option value="${item.value}">${item.name}</option>`;
   });
   citySelect.innerHTML = str;
+  if (mapCards) {
+    let html = "";
+    cityData.forEach((item) => {
+      html += `
+      <div class="card_map">
+        <i class="fas fa-map-marker-alt"></i>
+        <h4>${item.name}</h4>
+        <div class="img_container">
+          <img src="/images/city/${item.value}.png" alt="${item.name}" />
+        </div>
+      </div>
+      `;
+    });
+    mapCards.innerHTML = html;
+  }
+  for (let i = 0; i <= 21; i += 3) {
+    mapCards.children.item(i).classList.add("grid_1");
+  }
+  for (let i = 1; i <= 21; i += 3) {
+    mapCards.children.item(i).classList.add("grid_2");
+  }
+  console.log(mapCards, mapCards.item);
+  // for ( let i = 0 ; i < mapCards.length)
 }
-select();
+getCity();
 
-// 顯示公車列表
-const routeSelect = document.querySelector("#routeSelect");
-const getBus = (e) => {
+/** 預設顯示 */
+const setDefault = async () => {
+  if (activityCards) {
+    type = "Activity";
+    limitNum = 10;
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}?$top=${limitNum}&$format=JSON`;
+  }
+  try {
+    let html = "";
+    const res = await fetch(url, {
+      headers: getAuthorizationHeader(),
+    });
+    const data = await res.json();
+    console.log(data);
+    data.forEach((d) => {
+      // console.log(d.Name, d.Picture.PictureUrl1);
+      if (d.Picture.PictureUrl1) {
+        html += `
+      <div class="card_activity">
+      <div class="activity_img">
+        <img
+          src="${d.Picture.PictureUrl1}"
+          alt="${d.Name}"
+        />
+      </div>
+      <div class="activity_content">
+        <h4 class="activity_title">
+        ${d.Name}
+        </h4>
+        <p>
+        ${d.Description}...
+        </p>
+      </div>
+      <div class="info">
+        <span
+          ><i class="text_primary fas fa-map-marker-alt"></i
+          >&nbsp;${d.City}</span
+        >
+        <button class="btn btn_outline_primary">活動詳情</button>
+      </div>
+    </div>
+        `;
+      }
+    });
+    activityCards.innerHTML = html;
+  } catch (err) {
+    console.log(err);
+  }
+};
+setDefault();
+
+/** 預設顯示
+ * 預設顯示熱門美食 */
+axios
+  .get(
+    "https://ptx.transportdata.tw/MOTC/v2/Tourism/Restaurant?$top=10&$format=JSON",
+    {
+      headers: getAuthorizationHeader(),
+    }
+  )
+  .then(function (response) {
+    const data = [];
+    let html = "";
+    // 1. get JSON data
+    data.push(...response.data);
+    // console.log(data);
+    // 2. 熱門美食
+    data.forEach((d) => {
+      // console.log(d.Name, d.Picture.PictureUrl1);
+      html += `
+        <div class="card">
+        <div class="img_container">
+          <img
+            src="${d.Picture.PictureUrl1}"
+            alt="${d.Name}"
+          />
+        </div>
+        <div class="card_content">
+          <p>${d.Name}</p>
+          <div>
+            <i class="text_primary fas fa-map-marker-alt"></i>
+            <span>${d.Address}</span>
+          </div>
+        </div>
+      </div>
+        `;
+    });
+    foodCards.innerHTML = html;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+/** 預設顯示熱門住宿 */
+axios
+  .get(
+    "https://ptx.transportdata.tw/MOTC/v2/Tourism/Hotel?$top=10&$format=JSON",
+    {
+      headers: getAuthorizationHeader(),
+    }
+  )
+  .then(function (response) {
+    const data = [];
+    let html = "";
+    // 1. get JSON data
+    data.push(...response.data);
+    // console.log(data);
+    // 2. 熱門美食
+    data.forEach((d) => {
+      // console.log(d.Name, d.Picture.PictureUrl1);
+      if (d.Name && d.Picture.PictureUrl1) {
+        html += `
+        <div class="card">
+        <div class="img_container">
+          <img
+            src="${d.Picture.PictureUrl1}"
+            alt="${d.Name}"
+          />
+        </div>
+        <div class="card_content">
+          <p>${d.Name}</p>
+          <div>
+            <i class="text_primary fas fa-map-marker-alt"></i>
+            <span>${d.Address}</span>
+          </div>
+        </div>
+      </div>
+        `;
+      }
+    });
+    hotelCards.innerHTML = html;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+/** 篩選器
+ *  1. get the form data : eventListner
+ *  2. set URL : setURL()
+ *  3. fetch API
+ *  4. add element to layout
+ */
+const setURL = () => {
+  if (type && !city && !keywords) {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}?$top=40&$format=JSON`;
+  } else if (type && city && !keywords) {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}/${city}?$top=40&$format=JSON`;
+  } else if (type && !city && keywords) {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}?$filter=contains(Description,'${keywords}')&$top=40&$format=JSON`;
+  } else if (type && city && keywords) {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}/${city}?$filter=contains(Name,'${keywords}')&$top=40&$format=JSON`;
+  } else {
+    alert("請選擇類別");
+  }
+  console.log(url);
+};
+
+const getData = async (e) => {
+  let html = "";
+
+  /** 取得表單資料 */
+  e.preventDefault();
+  console.log(city, type, keywords);
+
+  /** 依類別取得資料 */
+  setURL();
+  try {
+    const res = await fetch(url, {
+      headers: getAuthorizationHeader(),
+    });
+    const data = await res.json();
+    console.log(data);
+    /** 清空多餘的標題 */
+    if (allTopics.length >= 1) {
+      for (let i = 1; i < allTopics.length; i++) {
+        console.log(i);
+        allTopics.item(i).remove();
+      }
+    }
+    /** 渲染標題 */
+    if (type === "Restaurant") {
+      html += `
+        <h3>
+          <img src="/images/Icon/Rectangle.png" alt="" />
+          熱門美食
+        </h3>
+        <div class="cards cards_food">`;
+    } else if (type === "Hotel") {
+      html += `
+        <h3>
+          <img src="/images/Icon/Rectangle.png" alt="" />
+          熱門景點
+        </h3>
+        <div class="cards cards_hotel">`;
+    } else if (type === "Activity") {
+      html += `
+        <h3>
+          <img src="/images/Icon/Triangle.png" alt="" />
+          熱門活動
+        </h3> 
+        <div class="cards cards_activity">
+      `;
+    }
+    /** 渲染內容 */
+    if (type === "Restaurant" || type === "Hotel") {
+      data.forEach((d) => {
+        if (d.Name && d.Picture.PictureUrl1) {
+          console.log(d.Name, d.Picture.PictureUrl1);
+          html += `
+        <div class="card">
+          <div class="img_container">
+            <img
+              src="${d.Picture.PictureUrl1}"
+              alt="${d.Name}"
+            />
+          </div>
+          <div class="card_content">
+            <p>${d.Name}</p>
+            <div>
+              <i class="text_primary fas fa-map-marker-alt"></i>
+              <span>${d.Address}</span>
+            </div>
+          </div>
+        </div>
+        `;
+        }
+      });
+      html += `</div>`;
+      topic.innerHTML = html;
+    } else if (type === "Activity") {
+      data.forEach((d) => {
+        // console.log(d.Name, d.Picture.PictureUrl1);
+        if (d.Picture.PictureUrl1) {
+          html += `
+        <div class="card_activity">
+        <div class="activity_img">
+          <img
+            src="${d.Picture.PictureUrl1}"
+            alt="${d.Name}"
+          />
+        </div>
+        <div class="activity_content">
+          <h4 class="activity_title">
+          ${d.Name}
+          </h4>
+          <p>
+          ${d.Description}...
+          </p>
+        </div>
+        <div class="info">
+          <span
+            ><i class="text_primary fas fa-map-marker-alt"></i
+            >&nbsp;${d.City}</span
+          >
+          <button class="btn btn_outline_primary">活動詳情</button>
+        </div>
+      </div>
+          `;
+        }
+      });
+      html += `</div>`;
+      topic.className = "topic topic_activity";
+      topic.innerHTML = html;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+typeSelect.addEventListener("change", (e) => {
+  type = e.target.value;
+});
+citySelect.addEventListener("change", (e) => {
   city = e.target.value;
-  //console.log("city", city);
-  if (city) {
-    axios
-      .get(
-        `https://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/${city}?$format=JSON`,
-        {
-          headers: getAuthorizationHeader(),
-        }
-      )
-      .then(function (response) {
-        const data = [];
-        data.push(...response.data);
-        let str = '<option value="">選擇路線</option>';
-        data.forEach((d) => {
-          str += `<option value="${d.RouteName.Zh_tw}">${d.RouteName.Zh_tw}</option>`;
-        });
-        routeSelect.innerHTML = str;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-};
-
-// 設定搜尋路線
-const setRoute = (e) => {
-  routeName = e.target.value;
-};
-
-// 顯示搜尋結果
-const searchBus = document.querySelector("#searchBus");
-const routeGroup = document.querySelector("div.route_group");
-const showRoute = () => {
-  if (routeName) {
-    console.log(city, routeName);
-    axios
-      .get(
-        `https://ptx.transportdata.tw/MOTC//v2/Bus/EstimatedTimeOfArrival/City/${city}/${routeName}?&$format=JSON`,
-        {
-          headers: getAuthorizationHeader(),
-        }
-      )
-      .then(function (response) {
-        /* 取得到站時間資料 */
-        const data = [];
-        data.push(...response.data);
-        console.log(data);
-        /* 排列資料 */
-        let str = "<p>請選擇乘車路線</p>";
-        data.forEach((d) => {
-          if (d.EstimateTime > 0) {
-            console.log(
-              `站名: ${d.StopName.Zh_tw} , 預估時間: ${d.EstimateTime}, 到站時間: ${d.DataTime}`
-            );
-            str += `
-            <div class="dus_stop">
-              <p class="btn btn_outline_dark">${d.EstimateTime}</p>
-              <p class="routeName">${d.StopName.Zh_tw}</p>
-            </div>`;
-          } else {
-            console.log(`站名:${d.StopName.Zh_tw}`);
-            str += `
-            <div class="dus_stop">
-              <p class="btn btn_outline_dark">未發車</p>
-              <p class="routeName">${d.StopName.Zh_tw}</p>
-            </div>`;
-          }
-          // str += `<option value="${d.RouteID}">${d.RouteName.Zh_tw}</option>`;
-        });
-        /* 加入畫面 */
-        routeGroup.innerHTML = str;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-};
-
-citySelect.addEventListener("change", getBus);
-routeSelect.addEventListener("change", setRoute);
-searchBus.addEventListener("click", showRoute);
-
-// // API
-// axios
-//   .get(
-//     "https://ptx.transportdata.tw/MOTC/v2/Rail/TRA/Station?$top=10&$format=JSON",
-//     {
-//       headers: getAuthorizationHeader(),
-//     }
-//   )
-//   .then(function (response) {
-//     const data = [];
-//     const stationList = document.querySelector("ul.stationList");
-//     // 1. get JSON data
-//     data.push(...response.data);
-//     // 2.
-//     console.log(data[1].StationName.Zh_tw);
-//     const stationName = data[1].StationName.Zh_tw;
-//     stationList.innerHTML = `<li class="btn btn_primary">${stationName}</li>`;
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+});
+input.addEventListener("change", (e) => {
+  keywords = e.target.value;
+});
+search.addEventListener("click", getData);
 
 function getAuthorizationHeader() {
   let AppID = "246448c88bce42bfa303e9edd65e4c4f";
